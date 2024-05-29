10e4fb494054fa573becce91b744ae1100f8d666
AlexPetrov88 <112386979+AlexPetrov88@users.noreply.github.com>
2024-05-29 15:02:06 +0300

    Add some settings
    Add some of Tailwind settings

--- location-search/src/App.tsx
+++ location-search/src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import { Place } from './api/Place';
+import LocationsSearch from './components/LocationsSearch';
+import Map from './components/Map';
+
 function App() {
+    const [place, setPlace] = useState<Place | null>(null);
+
     return (
         <>
-            <div className="m-10">Hi there!</div>
+            <div className="h-screen w-screen grid grid-cols-12">
+                <div className="col-span-3 p-2">
+                    <LocationsSearch />
+                </div>
+                <div className="col-span-9">
+                    <Map place={place} />
+                </div>
+            </div>
         </>
     );
 }
